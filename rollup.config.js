f9877c80229d5b9123ebd2a1476528fdb91725ca
Bill Neff <BNeff@synacor.com>
2019-03-22 16:34:17 -0400

    getWrappedComponent fix how rollup gets the current format

--- rollup.config.js
+++ rollup.config.js
@@ -2,28 +2,29 @@
 import memory from 'rollup-plugin-memory';
 import buble from 'rollup-plugin-buble';
 
-var FORMAT = process.env.FORMAT;
-
-export default {
-	external: [
-		'preact',
-		'dlv'
-	],
-	output: {
-		strict: false,
-		exports: FORMAT==='es' ? null : 'named',
-		globals: {
-			preact: 'preact',
-			dlv: 'dlv'
-		}
-	},
-	plugins: [
-		FORMAT!=='es' && memory({
-			path: 'src/entry.js',
-			contents: "export { default } from './index';"
-		}),
-		buble({
-			jsx: 'h'
-		})
-	].filter(Boolean)
-};
+export default function(config) {
+	let format = config.format;
+	return {
+		external: [
+			'preact',
+			'dlv'
+		],
+		output: {
+			strict: false,
+			exports: format==='es' ? null : 'named',
+			globals: {
+				preact: 'preact',
+				dlv: 'dlv'
+			}
+		},
+		plugins: [
+			format!=='es' && memory({
+				path: 'src/entry.js',
+				contents: "export { default } from './index';"
+			}),
+			buble({
+				jsx: 'h'
+			})
+		].filter(Boolean)
+	};
+}

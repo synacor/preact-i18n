83693a3db69b3cddbfd0d1c5ad043e4aa1271084
Stefanos Mousafeiris <msfstef@gmail.com>
2021-05-10 19:45:21 +0300

    Some test cases for pluralisation
    Checking the main pluralisation conventions work

--- test/lib/translate.js
+++ test/lib/translate.js
@@ -28,6 +28,29 @@ describe('translate', () => {
 	it('should translate <Text /> components that exist in field values when working on templated strings', () => {
 		expect(translate('foo.bar', undefined, { foo: { bar: 'hello{{c.d}}' }, e: 'World' }, { c: { d: <Text id="e" /> } })).to.equal('helloWorld');
 	});
+	
+	it('should pluralise for none/one/many pluralisation keys', () => {
+		expect(translate('foo.bar', undefined, { foo: { none: 'none', one: 'one', many: 'many' } }, undefined, 0)).to.equal('none');
+		expect(translate('foo.bar', undefined, { foo: { none: 'none', one: 'one', many: 'many' } }, undefined, 1)).to.equal('one');
+		expect(translate('foo.bar', undefined, { foo: { none: 'none', one: 'one', many: 'many' } }, undefined, 2)).to.equal('many');
+		expect(translate('foo.bar', undefined, { foo: { none: 'none', one: 'one', many: 'many' } }, undefined, 100)).to.equal('many');
+	});
+	
+	it('should pluralise for zero/one/other pluralisation keys', () => {
+		expect(translate('foo.bar', undefined, { foo: { zero: 'zero', one: 'one', other: 'other' } }, undefined, 0)).to.equal('zero');
+		expect(translate('foo.bar', undefined, { foo: { zero: 'zero', one: 'one', other: 'other' } }, undefined, 1)).to.equal('one');
+		expect(translate('foo.bar', undefined, { foo: { zero: 'zero', one: 'one', other: 'other' } }, undefined, 2)).to.equal('other');
+		expect(translate('foo.bar', undefined, { foo: { zero: 'zero', one: 'one', other: 'other' } }, undefined, 100)).to.equal('other');
+	});
+	
+	it('should pluralise for singular/plural pluralisation keys', () => {
+		// assume 0 is a plural form if using the singular/plural pluralisation convention
+		expect(translate('foo.bar', undefined, { foo: { singular: 'singular', plural: 'plural' } }, undefined, 0)).to.equal('plural');
+		
+		expect(translate('foo.bar', undefined, { foo: { singular: 'singular', plural: 'plural' } }, undefined, 1)).to.equal('singular');
+		expect(translate('foo.bar', undefined, { foo: { singular: 'singular', plural: 'plural' } }, undefined, 2)).to.equal('plural');
+		expect(translate('foo.bar', undefined, { foo: { singular: 'singular', plural: 'plural' } }, undefined, 100)).to.equal('plural');
+	});
 
 
 });
